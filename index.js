94b9dff78ced7084c8c8c02a0d98165405be4adc
Mustafa Oransel <oransel@users.noreply.github.com>
2018-05-29 09:07:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,7 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp((fastify, opts, next) => {
-  const lib = opts.library || 'ws'
-
-  if (lib !== 'ws' && lib !== 'uws') return next(new Error('Invalid "library" option'))
-
-  const WebSocketServer = require(lib).Server
+  const WebSocketServer = require('ws').Server
   const wss = new WebSocketServer({
     server: fastify.server
   })
